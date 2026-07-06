7e611bd9190f2034e5a19085af5496c87536fbf5
Mohamed ayman <gam8518@gmail.com>
2026-07-06 19:11:37 +0300

    feat: add dynamic sitemap using next.js sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://mohamedayman-dev.vercel.app",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
+      url: 'https://mohamedayman-dev.vercel.app',
+      lastModified: new Date('2026-07-04T18:43:22.603Z'),
+      changeFrequency: 'yearly',
       priority: 1,
     },
   ];
