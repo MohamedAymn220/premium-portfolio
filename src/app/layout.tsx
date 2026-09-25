b2b11b05adcf0f0e493e2989e2560c4005eae91a
Mohamed ayman <gam8518@gmail.com>
2026-09-25 11:53:08 +0300

    chore: update OG image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,8 +64,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/og-image.png",
-        width: 1725,
-        height: 912,
+        width: 1200,
+        height: 630,
         alt: "Mohamed Ayman Portfolio Premium Preview",
       },
     ],
